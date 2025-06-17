410b2bd66ae550b480a756f3076e28150826b193
theos-the-dawg <motheo13@outlook.com>
2025-06-17 12:06:42 +0200

    adding-correct-route

--- api/index.py
+++ api/index.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+import uvicorn
 
 app = FastAPI()
 
-@app.post("/")
+@app.post("/sort-characters")
 async def sort_string(request: Request):
     try:
         payload = await request.json()
@@ -15,3 +16,5 @@ async def sort_string(request: Request):
         return JSONResponse(content={"word": sorted_chars}, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
